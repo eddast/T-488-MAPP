a14a85ba3070b910d5cef4914e638c1c7a40c406
Edda Steinunn Rúnarsdóttir <eddarunarsdottir@Eddas-MacBook-Air.local>
2017-12-06 21:31:06 +0000

    Week 3, day 3 code refactor and commenting

--- Projects/Week 2 - Android/IOSWeek1/IOSWeek1.Android/MovieListActivity.cs
+++ Projects/Week 2 - Android/IOSWeek1/IOSWeek1.Android/MovieListActivity.cs
@@ -14,27 +14,42 @@ namespace IOSWeek1.Droid
     public class MovieListActivity : Activity
     {
         private List<MovieModel> _movies;
+        private ListView _listView;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            SetContentView(Resource.Layout.SearchResults);
-            var listView = this.FindViewById<ListView>(Resource.Id.listView);
+            base.OnCreate(savedInstanceState); // call to base func
 
-            base.OnCreate(savedInstanceState);
+            // Set content view to XML layout and extract list view
+            SetContentView(Resource.Layout.SearchResults);
+            _listView = this.FindViewById<ListView>(Resource.Id.listView);
 
             // Extract values passed down via Intent
             var jsonMovies = this.Intent.GetStringExtra("movieList");
             this._movies = JsonConvert.DeserializeObject<List<MovieModel>>(jsonMovies);
-            if (_movies.Count == 0) { Toast.MakeText(ApplicationContext, "No results", ToastLength.Long).Show(); }
+            if (_movies.Count == 0) {
+                
+                // Feedback provided if no results were found
+                Toast.MakeText(ApplicationContext, "No results", ToastLength.Long).Show();
+            }
 
-            listView.Adapter = new MovieListAdapter(this, this._movies);
-
+            // Set listview adapter to movie list adapter that builds up movie list
+            // Set toolbar as "action bar" and give it appropriate title
+            _listView.Adapter = new MovieListAdapter(this, this._movies);
             var toolbar = this.FindViewById<Toolbar>(Resource.Id.toolbar);
             this.SetActionBar(toolbar);
             this.ActionBar.Title = "Movie Search";
 
-            listView.ItemClick += (sender, args) => {
-                
+            // Listens to cell click and "redirects" user to detail page
+            ListenToCellClicks();
+        }
+
+        // Listens for cell click and starts the detail movie view activity
+        // passes down movie information in json bundle for detail view
+        public void ListenToCellClicks()
+        {
+            this._listView.ItemClick += (sender, args) => {
+
                 var intent = new Intent(this, typeof(MovieDetailActivity));
                 intent.PutExtra("movie", JsonConvert.SerializeObject(_movies[args.Position]));
                 this.StartActivity(intent);
