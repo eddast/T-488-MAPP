34aa63b4c10473b64e60811cd07dd243a90449ea
Edda Steinunn Rúnarsdóttir <eddarunarsdottir@Eddas-MacBook-Air.local>
2017-12-06 21:04:50 +0000

    Week 3, day 3 code refactor and commenting

--- Projects/Week 2 - Android/IOSWeek1/IOSWeek1.Android/TabsFragmentPagerAdapter.cs
+++ Projects/Week 2 - Android/IOSWeek1/IOSWeek1.Android/TabsFragmentPagerAdapter.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Support.V4.App;
-using Android.Views;
-using Android.Widget;
+﻿using Android.Support.V4.App;
 using Java.Lang;
 using Fragment = Android.Support.V4.App.Fragment;
 using FragmentManager = Android.Support.V4.App.FragmentManager;
 
 namespace IOSWeek1.Droid
 {
+    // Represents each page as a Fragment as long as user can return to page
     public class TabsFragmentPagerAdapter : FragmentPagerAdapter
     {
-
+        // Keeps track of fragments and their titles
         private readonly Fragment[] _fragments;
         private readonly ICharSequence[] _titles;
 
@@ -28,6 +18,7 @@ namespace IOSWeek1.Droid
             this._titles = titles;
         }
 
+        // Override necessary functions for a working fragment pager adapter
         public override ICharSequence GetPageTitleFormatted(int position) { return this._titles[position]; }
         public override Fragment GetItem(int position) { return this._fragments[position]; }
         public override int Count => this._fragments.Length;
